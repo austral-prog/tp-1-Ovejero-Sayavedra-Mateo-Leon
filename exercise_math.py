d6888d946fdf547a5292909bca43036881e655dc
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-03-21 10:45:50 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,12 @@
 def math():
-    a = 57
-    b = 7
+    print("Ejercicio math hecho")
+    num1 = 57
+    num2 = 7
+    print(num1 + num2)
+    print(num1 - num2)
+    print(num1 * num2)
+    print((num1 + num2)/2)
+    print(num1 // num2)
+    print(num1 % num2)
+    print(num1 / num2)
+math()
